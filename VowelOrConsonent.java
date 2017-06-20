39ae7f9efc916983427be2a76538d9aefcb7bed8
Kausiha <ruks.kausiha97@gmail.com>
2017-06-20 14:53:11 +0530

    Create VowelOrConsonent.java

---
+++ VowelOrConsonent.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class vowelornot{
+ public static void main(String [] args){
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter a Character");
+  char chr=s.next().charAt(0);
+  if(chr=='a'||chr=='e'||chr=='i'||chr=='o'||chr=='u'||chr=='A'||chr=='E'||chr=='I'||chr=='O'||chr=='U'){
+  System.out.println(chr+" is a Vowel");
+ }
+  else
+  System.out.println(chr+" is a Consonent");
+}
+}
