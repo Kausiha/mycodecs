0ab4fdbec2b7ef5592e1a22fac0e445c91bd04a5
Kausiha <ruks.kausiha97@gmail.com>
2017-06-20 15:12:41 +0530

    Update FindNumber.java

--- FindNumber.java
+++ FindNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class number{
+public class FindNumber{
  public static void main(String [] args){
   Scanner s=new Scanner(System.in);
   System.out.println("Enter a Number");
