6b71a675e19649edbc98f8a895b3ec1d93e684a9
Kausiha <ruks.kausiha97@gmail.com>
2017-06-20 15:04:52 +0530

    Update VowelOrConsonent.java

--- VowelOrConsonent.java
+++ VowelOrConsonent.java
@@ -5,10 +5,14 @@ public class vowelornot{
   Scanner s=new Scanner(System.in);
   System.out.println("Enter a Character");
   char chr=s.next().charAt(0);
-  if(chr=='a'||chr=='e'||chr=='i'||chr=='o'||chr=='u'||chr=='A'||chr=='E'||chr=='I'||chr=='O'||chr=='U'){
+  if(!Character.isDigit(chr))
+  {
+  if(chr=='a'||chr=='e'||chr=='i'||chr=='o'||chr=='u'||chr=='A'||chr=='E'||chr=='I'||chr=='O'||chr=='U')
   System.out.println(chr+" is a Vowel");
- }
   else
   System.out.println(chr+" is a Consonent");
+  }
+  else
+  System.out.println("Enter only Alphabets");
 }
 }
