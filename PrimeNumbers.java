38c7b8b62cd5fd843a9e9920a02fb54edb1477d7
Kausiha <ruks.kausiha97@gmail.com>
2017-06-22 16:42:30 +0530

    Create PrimeNumbers.java

---
+++ PrimeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int first = s.nextInt();
+		int last = s.nextInt();
+		int k = 0, count = 0, z = (first + 1) - (last - 1);
+		for (int i = first + 1; i < last; i++) {
+			for (int j = 2; j < i / 2; j++) {
+				if (i % j == 0) {
+					k = -1;
+					break;
+				}
+			}
+			if (k == 0)
+				count++;
+			k = 0;
+
+		}
+		System.out.println(count);
+
+	}
+
+}
