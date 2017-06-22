3331ce2063dc5cc14ff9088e37f88bcf295a351a
Kausiha <ruks.kausiha97@gmail.com>
2017-06-22 16:40:08 +0530

    Create StringToIntegerConversion.java

---
+++ StringToIntegerConversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class StringToIntegerConversion {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a String:");
+		String str = s.next();
+		int val = 0, s1 = 1;
+		for (int i = 0; i < str.length(); i++) {
+			val = str.charAt(i);
+			System.out.println(+val + "");
+		}
+		System.out.println("is the Integer for the String " + str);
+	}
+}
