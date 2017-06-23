104ac1b0d3ab41cd1fb9965d29825c1fa4b73e7b
Kausiha <ruks.kausiha97@gmail.com>
2017-06-23 09:00:22 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,15 +1,19 @@
 package sumOfTwoNumbers;
+
 import java.util.Scanner;
+
 public class Factorial {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter a Number:");
 		int num = s.nextInt();
 		int f = 1;
-		for (int i = num; i > 0; i--) {
-			f *= i;
-		}
-		System.out.println("The factorial of the Number is: " + f);
+		if (num > 1) {
+			for (int i = num; i > 0; i--) {
+				f *= i;
+			}
+			System.out.println("The factorial of the Number is: " + f);
+		} else
+			System.out.print("Enter a number greater than 0");
 	}
-
 }
