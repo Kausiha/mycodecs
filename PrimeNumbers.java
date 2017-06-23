85335ec505ba1615e366f01893c34d8e82c9fbc6
Kausiha <ruks.kausiha97@gmail.com>
2017-06-23 09:31:27 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -6,18 +6,21 @@ public class PrimeNumbers {
 		int first = s.nextInt();
 		int last = s.nextInt();
 		int k = 0, count = 0, z = (first + 1) - (last - 1);
-		for (int i = first + 1; i < last; i++) {
-			for (int j = 2; j < i / 2; j++) {
-				if (i % j == 0) {
-					k = -1;
-					break;
+		if ((first > 0) && (last > 0)) {
+			for (int i = first + 1; i < last; i++) {
+				for (int j = 2; j < i / 2; j++) {
+					if (i % j == 0) {
+						k = -1;
+						break;
+					}
 				}
-			}
-			if (k == 0)
-				count++;
-			k = 0;
+				if (k == 0)
+					count++;
+				k = 0;
 
-		}
+			}
+		} else
+			System.out.println("Enter a number greater than 0");
 		System.out.println(count);
 
 	}
